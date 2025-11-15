4d046fba5c120dec373e44d76b696478453b2c91
huyen7 <hamyhuyen7@gmail.com>
2025-11-15 11:38:39 +0700

    Tao trang cap nhat lien he

--- src/router/index.js
+++ src/router/index.js
@@ -13,6 +13,13 @@ const routes = [
         name: "notfound",
         component: () => import("@/views/NotFound.vue"),
     },
+
+    {
+        path: "/contact/:id",
+        name: "contact.edit",
+        component: () => import("@/views/ContactEdit.vue"),
+        props: true //Truyền các biến trong $router.params vào làm props
+    },
 ];
 
 const router = createRouter({
